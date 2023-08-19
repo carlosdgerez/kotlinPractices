32b09f7749ac7c234ee93a066e98b1f639ee52a2
carlosdgerez <carlosgerez@byui.edu>
2023-08-19 23:21:56 +0200

    Fifth

--- kotlin1/src/main/kotlin/Main.kt
+++ kotlin1/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println("Hello World!")
+    println("Hello Wogterrld!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
